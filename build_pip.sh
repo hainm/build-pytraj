e372d32d9b0970957e1de66f5cec5768b88153a7
Hai Nguyen <hainm.comp@gmail.com>
2016-11-13 18:46:29 -0500

    more

--- build_pip.sh
+++ build_pip.sh
@@ -25,7 +25,7 @@ platform=`uname`
 if [ "$platform" = 'Linux' ]; then
     python scripts/install_libcpptraj.py -openmp
 else
-    CPPTRAJ_COMPILER_OPTION=clang python scripts/install_libcpptraj.py
+    python scripts/install_libcpptraj.py
 fi
 
 cd dist
