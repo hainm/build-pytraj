1b18da2ea2fa7314338eb6f77b08b01c68ab81df
Hai Nguyen <hainm.comp@gmail.com>
2016-11-18 16:58:44 -0500

    remove netcdf-3

--- ci/build_deps.sh
+++ ci/build_deps.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 
-netcdf=ftp://ftp.pbone.net/mirror/ftp5.gwdg.de/pub/opensuse/repositories/home:/marcindulak/CentOS_CentOS-5/x86_64/netcdf4-devel-4.1.2-23.1.x86_64.rpm
+netcdf=ftp://ftp.pbone.net/mirror/ftp5.gwdg.de/pub/opensuse/repositories/home:/marcindulak/CentOS_CentOS-5/x86_64/netcdf4-4.1.2-23.1.x86_64.rpm
+netcdfdev=ftp://ftp.pbone.net/mirror/ftp5.gwdg.de/pub/opensuse/repositories/home:/marcindulak/CentOS_CentOS-5/x86_64/netcdf4-devel-4.1.2-23.1.x86_64.rpm
 yum -y install bzip2
 yum -y install blas-devel
 yum -y install lapack-devel
 yum -y install arpack-devel
 yum -y install zlib-devel
 yum -y install bzip2-devel
-yum -y install netcdf-devel
 yum -y install gfortran
 yum -y install libgfortran
 yum -y install fftw3-devel
 wget $netcdf
+wget $netcdfdev
+rpm -i ./netcdf4-4.1.2-23.1.x86_64.rpm
 rpm -i ./netcdf4-devel-4.1.2-23.1.x86_64.rpm
