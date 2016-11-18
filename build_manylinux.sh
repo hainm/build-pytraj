7d8c9367c14a93c98f44d01d233f50c18dccc205
Hai Nguyen <hainm.comp@gmail.com>
2016-11-18 01:25:17 -0500

    damn env

--- build_manylinux.sh
+++ build_manylinux.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 export PYVER=$PYVER
+
+if [ "$CIRCLECI" = 'true' ]; then
+    export DOCKER_IMAGE=quay.io/pypa/manylinux1_x86_64
+    export PYVER=cp35-cp35m
+fi
 docker run -e "PYVER=$PYVER" --rm -v `pwd`:/io $DOCKER_IMAGE $PRE_CM bash /io/travis-ci/build_wheels.sh
 ls wheelhouse/
