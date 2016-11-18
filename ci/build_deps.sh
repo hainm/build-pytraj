182f5e54e2a19deefd3d6f119518f7fcacb8e469
Hai Nguyen <hainm.comp@gmail.com>
2016-11-18 16:43:05 -0500

    netcdf4

--- ci/build_deps.sh
+++ ci/build_deps.sh
@@ -8,5 +8,7 @@ yum -y install zlib-devel
 yum -y install bzip2-devel
 yum -y install netcdf-devel
 yum -y install gfortran
+yum -y install libgfortran
 yum -y install fftw3-devel
-
+wget ftp://ftp.pbone.net/mirror/ftp5.gwdg.de/pub/opensuse/repositories/home:/marcindulak/CentOS_CentOS-5/x86_64/netcdf4-4.1.2-23.1.x86_64.rpm
+rpm -i ./tcdf4-4.1.2-23.1.x86_64.rpm
