269047893720a868e8346be168a7167bb64a7568
Hai Nguyen <hainm.comp@gmail.com>
2016-11-18 14:18:30 -0500

    more

--- build_manylinux.sh
+++ build_manylinux.sh
@@ -3,8 +3,8 @@ export PYVER=$PYVER
 
 if [ "$CIRCLECI" = 'true' ]; then
     export DOCKER_IMAGE=quay.io/pypa/manylinux1_x86_64
-    # export PYVER=cp35-cp35m
-    export USE_MINICONDA='true'
+    export PYVER=cp35-cp35m
+   #  export USE_MINICONDA='true'
 fi
 docker run -e "PYVER=$PYVER" -e "USE_MINICONDA=${USE_MINICONDA}" --rm -v `pwd`:/io $DOCKER_IMAGE $PRE_CM bash /io/ci/build_wheels.sh
 ls wheelhouse/
