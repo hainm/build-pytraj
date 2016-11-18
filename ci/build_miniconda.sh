ecd1a210a40298d9c57ed238041b74d65305c87f
Hai Nguyen <hainm.comp@gmail.com>
2016-11-18 16:03:18 -0500

    more

--- ci/build_miniconda.sh
+++ ci/build_miniconda.sh
@@ -1,18 +1,13 @@
 #!/bin/sh
 
-if [ "$TRAVIS_OS_NAME" = "osx" ]; then
-    wget http://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O miniconda.sh;
-else
-    wget http://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
-fi
-
+wget http://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
 bash miniconda.sh -b
-
 export PATH=$HOME/miniconda3/bin:$PATH
 export LD_LIBRARY_PATH=$HOME/miniconda3/lib:$LD_LIBRARY_PATH
 
 conda install -y  \
     cython \
+    conda-build \
     numpy \
     nomkl \
     openblas \
