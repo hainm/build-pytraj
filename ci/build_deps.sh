05ff4cf0f4386904f0f5b14ba38b31c3f5151aee
Hai Nguyen <hainm.comp@gmail.com>
2016-11-18 16:47:17 -0500

    netcdf-dev

--- ci/build_deps.sh
+++ ci/build_deps.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+netcdf=ftp://ftp.pbone.net/mirror/ftp5.gwdg.de/pub/opensuse/repositories/home:/marcindulak/CentOS_CentOS-5/x86_64/netcdf4-devel-4.1.2-23.1.x86_64.rpm
 yum -y install bzip2
 yum -y install blas-devel
 yum -y install lapack-devel
@@ -10,5 +11,5 @@ yum -y install netcdf-devel
 yum -y install gfortran
 yum -y install libgfortran
 yum -y install fftw3-devel
-wget ftp://ftp.pbone.net/mirror/ftp5.gwdg.de/pub/opensuse/repositories/home:/marcindulak/CentOS_CentOS-5/x86_64/netcdf4-4.1.2-23.1.x86_64.rpm
-rpm -i ./tcdf4-4.1.2-23.1.x86_64.rpm
+wget $netcdf
+rpm -i ./netcdf4-devel-4.1.2-23.1.x86_64.rpm
