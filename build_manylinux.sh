fd587fe01853a0b120ff6cdee51c75bd1125d241
Hai Nguyen <hainm.comp@gmail.com>
2016-11-18 01:19:40 -0500

    add permission

--- build_manylinux.sh
+++ build_manylinux.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 export PYVER=$PYVER
-docker run -e "PYVER=$PYVER" --rm -v `pwd`:/io $DOCKER_IMAGE $PRE_CMD /io/travis-ci/build_wheels.sh
+docker run -e "PYVER=$PYVER" --rm -v `pwd`:/io $DOCKER_IMAGE $PRE_CM bash /io/travis-ci/build_wheels.sh
 ls wheelhouse/
